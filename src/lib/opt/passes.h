0eb96685f544a644ef676e4d36adb773922e0540
Juneseo Chang <jschang0215@snu.ac.kr>
2023-05-06 17:39:07 +0900

    [Sprint 1] shift & constant add optimization (#12)
    * Shift optimization
    
    * Constant add/sub to incr/decr
    
    * Reflected Sprint 1 code review
    
    * Change original test case
    
    * Change original test case
    
    * Disable alive2
    
    * Fix test case

--- src/lib/opt/passes.h
+++ src/lib/opt/passes.h
@@ -1,6 +1,8 @@
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -8,6 +10,15 @@ using namespace llvm;
 class SimplePass : public PassInfoMixin<SimplePass> {
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+};
+
+class ShiftConstantAddPass : public PassInfoMixin<ShiftConstantAddPass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+
+private:
+  void shiftToOp(Instruction *I, Instruction::BinaryOps Operator,
+                  IRBuilder<> &Builder);
 };
 
 class OraclePass : public PassInfoMixin<OraclePass> {
