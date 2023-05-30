47dcb2b342ac3aa64e0c11c1b16a304c7e2933de
yoonshik1205 <yoonshik1205@gmail.com>
2023-05-30 18:15:48 +0900

    address reviews

--- src/lib/opt/pre_oracle.cpp
+++ src/lib/opt/pre_oracle.cpp
@@ -1,5 +1,23 @@
 #include "./pre_oracle.h"
 
+void processInstruction(Instruction &I, std::vector<StoreInst *> &stores) {
+  if (auto *SI = dyn_cast<StoreInst>(&I)) {
+    stores.push_back(SI);
+  } else if (isa<LoadInst>(I) || isa<CallInst>(I) || I.isTerminator()) {
+    // load and call instructions can have unexpected dependencies, so the
+    // naive approach cannot move the store instructions past them
+    if (stores.size() < 3) {
+      // if less than three stores, no need to cluster
+      stores.clear();
+      return;
+    }
+    // move all instructions in stores to just before I
+    for (auto *SI : stores)
+      SI->moveBefore(&I);
+    stores.clear();
+  }
+}
+
 PreservedAnalyses PreOraclePass::run(Function &F,
                                      FunctionAnalysisManager &FAM) {
   bool changed = false;
@@ -7,27 +25,10 @@ PreservedAnalyses PreOraclePass::run(Function &F,
     // stores: list of encountered stores that have not been clustered
     std::vector<StoreInst *> stores;
     for (auto &I : BB) {
-      if (auto *SI = dyn_cast<StoreInst>(&I)) {
-        stores.push_back(SI);
-      } else if (isa<LoadInst>(I) || isa<CallInst>(I) || I.isTerminator()) {
-        // load and call instructions can have unexpected dependencies, so the
-        // naive approach cannot move the store instructions past them
-        if (stores.size() < 3) {
-          // if less than three stores, no need to cluster
-          stores.clear();
-          continue;
-        }
-        // move all instructions in stores to just before I
-        changed = true;
-        for (auto *SI : stores)
-          SI->moveBefore(&I);
-        stores.clear();
-      }
+      processInstruction(I, stores);
     }
   }
-  if (!changed)
-    return PreservedAnalyses::all();
-  return PreservedAnalyses::none();
+  return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
