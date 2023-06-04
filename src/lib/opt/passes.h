518d7804e48e9525ec6e704436700c53dd82ccd2
taro1265 <87560187+taro1265@users.noreply.github.com>
2023-06-04 11:11:29 +0900

    [Sprint3] change order of aloads (#49)
    * implement pass
    * apply code review

--- src/lib/opt/passes.h
+++ src/lib/opt/passes.h
@@ -16,6 +16,7 @@
 #include "./recursive_branch_condition.h"
 #include "./heap_promotion.h"
 #include "./malloc_free_reordering.h"
+#include "./aload_reordering.h"
 #include "./add_licm.h"
 #include "./switch_to_br.h"
 #include "./likely_branch_condition.h"
