e07c3bdbdf80615cf01848b3d6481fe993d077ac
taro1265 <87560187+taro1265@users.noreply.github.com>
2023-06-08 10:15:09 +0900

    [Wrap-up] modify pass (#62)
    modify pass

--- src/lib/opt/declare.cpp
+++ src/lib/opt/declare.cpp
@@ -21,23 +21,21 @@ PreservedAnalyses DeclarePass::run(Module &M, ModuleAnalysisManager &MAM) {
   Type *IntPtr64 = Type::getInt64PtrTy(Context);
 
   // aload
-  FunctionType *Aload1 = FunctionType::get(Int1, IntPtr1, true);
-  FunctionType *Aload8 = FunctionType::get(Int8, IntPtr8, true);
-  FunctionType *Aload16 = FunctionType::get(Int16, IntPtr16, true);
-  FunctionType *Aload32 = FunctionType::get(Int32, IntPtr32, true);
-  FunctionType *Aload64 = FunctionType::get(Int64, IntPtr64, true);
-  M.getOrInsertFunction("aload_i1", Aload1);
+  FunctionType *Aload8 = FunctionType::get(Int8, IntPtr8, false);
+  FunctionType *Aload16 = FunctionType::get(Int16, IntPtr16, false);
+  FunctionType *Aload32 = FunctionType::get(Int32, IntPtr32, false);
+  FunctionType *Aload64 = FunctionType::get(Int64, IntPtr64, false);
   M.getOrInsertFunction("aload_i8", Aload8);
   M.getOrInsertFunction("aload_i16", Aload16);
   M.getOrInsertFunction("aload_i32", Aload32);
   M.getOrInsertFunction("aload_i64", Aload64);
 
   // incr
-  FunctionType *Incr1 = FunctionType::get(Int1, Int1, true);
-  FunctionType *Incr8 = FunctionType::get(Int8, Int8, true);
-  FunctionType *Incr16 = FunctionType::get(Int16, Int16, true);
-  FunctionType *Incr32 = FunctionType::get(Int32, Int32, true);
-  FunctionType *Incr64 = FunctionType::get(Int64, Int64, true);
+  FunctionType *Incr1 = FunctionType::get(Int1, Int1, false);
+  FunctionType *Incr8 = FunctionType::get(Int8, Int8, false);
+  FunctionType *Incr16 = FunctionType::get(Int16, Int16, false);
+  FunctionType *Incr32 = FunctionType::get(Int32, Int32, false);
+  FunctionType *Incr64 = FunctionType::get(Int64, Int64, false);
   M.getOrInsertFunction("incr_i1", Incr1);
   M.getOrInsertFunction("incr_i8", Incr8);
   M.getOrInsertFunction("incr_i16", Incr16);
@@ -45,11 +43,11 @@ PreservedAnalyses DeclarePass::run(Module &M, ModuleAnalysisManager &MAM) {
   M.getOrInsertFunction("incr_i64", Incr64);
 
   // decr
-  FunctionType *Decr1 = FunctionType::get(Int1, Int1, true);
-  FunctionType *Decr8 = FunctionType::get(Int8, Int8, true);
-  FunctionType *Decr16 = FunctionType::get(Int16, Int16, true);
-  FunctionType *Decr32 = FunctionType::get(Int32, Int32, true);
-  FunctionType *Decr64 = FunctionType::get(Int64, Int64, true);
+  FunctionType *Decr1 = FunctionType::get(Int1, Int1, false);
+  FunctionType *Decr8 = FunctionType::get(Int8, Int8, false);
+  FunctionType *Decr16 = FunctionType::get(Int16, Int16, false);
+  FunctionType *Decr32 = FunctionType::get(Int32, Int32, false);
+  FunctionType *Decr64 = FunctionType::get(Int64, Int64, false);
   M.getOrInsertFunction("decr_i1", Decr1);
   M.getOrInsertFunction("decr_i8", Decr8);
   M.getOrInsertFunction("decr_i16", Decr16);
@@ -57,11 +55,11 @@ PreservedAnalyses DeclarePass::run(Module &M, ModuleAnalysisManager &MAM) {
   M.getOrInsertFunction("decr_i64", Decr64);
 
   // sum
-  FunctionType *i1_type = FunctionType::get(Int1, eight_i1, true);
-  FunctionType *i8_type = FunctionType::get(Int8, eight_i8, true);
-  FunctionType *i16_type = FunctionType::get(Int16, eight_i16, true);
-  FunctionType *i32_type = FunctionType::get(Int32, eight_i32, true);
-  FunctionType *i64_type = FunctionType::get(Int64, eight_i64, true);
+  FunctionType *i1_type = FunctionType::get(Int1, eight_i1, false);
+  FunctionType *i8_type = FunctionType::get(Int8, eight_i8, false);
+  FunctionType *i16_type = FunctionType::get(Int16, eight_i16, false);
+  FunctionType *i32_type = FunctionType::get(Int32, eight_i32, false);
+  FunctionType *i64_type = FunctionType::get(Int64, eight_i64, false);
 
   M.getOrInsertFunction("int_sum_i1", i1_type);
   M.getOrInsertFunction("int_sum_i8", i8_type);
